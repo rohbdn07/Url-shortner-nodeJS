8fdb87d33b3b9db78d9bd05bbb73046b4f23bac9
rohbdn07 <rohbdn07@users.noreply.github.com>
2020-10-31 01:32:42 +0200

    Partials folder created and use of Bootstrap

--- routes/urlRoute.js
+++ routes/urlRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/',(req,res)=>{
-    res.json('this is home route')
+    res.render('index')
 })
 
 module.exports=router;
