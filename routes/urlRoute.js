8fed4457c5bb406a81bef050ea11604a87476310
rohbdn07 <rohbdn07@users.noreply.github.com>
2020-11-01 02:51:00 +0200

    Apis are working fine. Same existing long Urls will not re-save to db.ok

--- routes/urlRoute.js
+++ routes/urlRoute.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const UrlShortner = require('../models/UrlShortner');
 
  
-//GET route
+//GET route(/) to get all the saved data...
 router.get('/', async(req,res)=>{
     try {
-        const data= await UrlShortner.find({});
-        console.log(data)
+        const data= await UrlShortner.find({}).sort({createdAt:'-1'});
+        //console.log(data)
          res.render('index',{
         urlShortners:data
     })
@@ -17,12 +17,16 @@ router.get('/', async(req,res)=>{
     }  
 })
 
-//POST route
-router.post('/shortUrls', async(req,res)=>{
-    //res.json('this is ok')
-    const urlShort = new UrlShortner();
-    urlShort.longUrl=req.body.longUrl;
-    try {
+//POST route (/shortUrls) to store Long Url to db...
+router.post('/shortUrls', async(req,res, next)=>{
+     try {
+        const duplicate= await UrlShortner.findOne({longUrl:req.body.longUrl});
+        if(duplicate) return(
+            console.log("Already exit url"),
+            res.redirect('/')
+        ) 
+        const urlShort = new UrlShortner();
+        urlShort.longUrl=req.body.longUrl;
         await urlShort.save();
         console.log('LongUrl is saved to db')
         res.redirect('/');
@@ -30,6 +34,22 @@ router.post('/shortUrls', async(req,res)=>{
         res.render('index');
         console.log('Unable to store the data', error)
     }
+
+})
+
+//GET route to shourturl params...
+router.get('/:shortUrl', async(req,res)=>{
+    const shortUrl= await UrlShortner.findOne({shortUrl: req.params.shortUrl});
+    if(!shortUrl) return res.sendstatus(404);
+   
+    try { 
+        shortUrl.clicks++
+        await shortUrl.save();
+        res.redirect(shortUrl.longUrl);
+    } catch (error) {
+        console.log(error)
+    }
+    
 
 })
 module.exports=router;
